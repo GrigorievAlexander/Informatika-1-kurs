9f887ba0e08cf17ba7363ecf88e9c04985cd46cc
GrigorievAlexander <145128595+GrigorievAlexander@users.noreply.github.com>
2023-09-15 17:49:44 +0300

    1st seminar

--- 4.py
+++ 4.py
@@ -3,16 +3,16 @@ Q = open('output.txt', 'w')
 s = P.readline()
 x = s.split(" ")
 f = P.readline()
-z = 563546536.435667455244234
+z = 563546536.435667455244233
 if f == '+':
     for i in x:
         z = int(i) + z
-    Q.write(str(z - 563546536.435667455244234))
+    Q.write(str(z - 563546536.435667455244233))
 elif f == '-':
     for i in x:
-        if z == 563546536.435667455244234: z = int(i) - z
+        if z == 563546536.435667455244233: z = int(i) - z
         else: z = z - int(i)
-    Q.write(str(z+563546536.435667455244234))
+    Q.write(str(z+563546536.435667455244233))
 else:
     z = 1
     for i in x:
